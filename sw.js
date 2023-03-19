b072d89dc9fa4c1713033feae997deb487df81b6
Ichsan-Abdalla <120088732+Ichsan-Abdalla@users.noreply.github.com>
2023-03-19 13:04:22 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const $versi = "v 0.0";
 const $caches = [
-	'.','index.html','style.css','script.js','Poppins-Regular.woff','icon.png','welcome.jpg','misi.jpg','3.jpg','x.jpg','offroad.jpg','rafting.jpg','paintball.jpg','fox.jpg','team.jpg','atv.jpg','harmoko.png'
+	'.','index.html','style.css','script.js','Poppins-Regular.woff','icon.png','welcome.jpg','misi.jpg','3.jpg','x.jpg','offroad.jpg','rafting.jpg','paintball.jpg','fox.jpg','team.jpg','atv.jpg','harmoko.png','google8f10dab510b8f12c.html'
 ];
 
 self.addEventListener('install', e => {
